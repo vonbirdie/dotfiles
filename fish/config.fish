613acccb00599bc3a62f5f496b4d1fa652b4b095
Andreas Falk <mail@andreasfalk.se>
2017-01-15 14:08:31 +0000

    Use a plain file instead of link to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,32 @@
-/home/andreas/dotfiles/config.fish
+
+# ----------------------------------------------------------------------
+# EDITOR
+# ----------------------------------------------------------------------
+
+set -gx EDITOR nvim
+
+# PAGER
+if command less
+  set -gx PAGER "less -FirSwX"
+  set -gx MANPAGER "less -FiRswX"
+else
+  set -gx PAGER more
+  set -gx MANPAGER "$PAGER"
+end
+
+# ----------------------------------------------------------------------
+# LS
+# ----------------------------------------------------------------------
+
+# we always pass these to ls(1)
+set -gx LS_COMMON "lhBG"
+
+# -------------------------------------------------------------------
+# USER SHELL ENVIRONMENT
+# -------------------------------------------------------------------
+
+# source ~/.shenv now if it exists
+if test -r ~/.shenv.fish
+  source ~/.shenv.fish
+end
+
