11f0bc34712835295a66c4ca270ff5aa9f470d49
Andreas Falk <mail@andreasfalk.se>
2019-07-20 15:24:19 +0100

    Use stow to install files

--- install.sh
+++ install.sh
@@ -4,25 +4,27 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-function set_rc {
-    [ -z "$1" ] && return
-    [ -z "$2" ] && return
+function set_rc() {
+  [ -z "$1" ] && return
+  [ -z "$2" ] && return
 
-    echo "Linking ${1} to ${2}..."
-
-    if [ -e "~/${2}" ]; then
-      mv -f "~/${2}" "~/${2}.bak"
-    fi
-
-    ln -sf "~/dotfiles/${1}" "~/${2}"
+  stow --verbose --restow --target "${1}" "${2}"
 }
 
-set_rc "bin" "bin"
-set_rc "fish" ".config/fish"
-set_rc "gitconfig" ".gitconfig"
-set_rc "inputrc" ".inputrc"
-set_rc "nvim" ".config/nvim"
-set_rc "tmux.conf" ".tmux.conf"
-set_rc "zshrc" ".zshrc"
+function main() {
+  local current_user="$(whoami)"
+  local user_homedir="$(getent passwd "${current_user}" | cut -d: -f6)"
+  echo "Running as ${current_user} and will install to ${user_homedir}"
+
+  set_rc "${user_homedir}/bin" "bin"
+  set_rc "${user_homedir}/.config/fish" "fish"
+  set_rc "${user_homedir}/.config/nvim" "nvim"
+  set_rc "${user_homedir}/" "git"
+  set_rc "${user_homedir}/" "readline"
+  set_rc "${user_homedir}/" "tmux"
+  set_rc "${user_homedir}/" "zsh"
+}
+
+main
 
 exit 0
