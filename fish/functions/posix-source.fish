63e85d8e283c523881102ad443628c9ade3d768c
Andreas Falk <mail@andreasfalk.se>
2019-07-21 15:23:52 +0100

    Remove leading and trailing quotation marks

--- fish/functions/posix-source.fish
+++ fish/functions/posix-source.fish
@@ -2,7 +2,7 @@
 function posix-source
   for line in (cat $argv | grep --invert-match --line-regexp --perl-regexp --regexp="[ \t]*(#.+)?")
     set varValues (echo $line |tr = \n)
-    set  --global --export $varValues[1] $varValues[2]
+    set  --global --export $varValues[1] (echo $varValues[2] | sed -e 's/^"//' | sed -e 's/"$//')
     set --show $varValues[1]
   end
 end
