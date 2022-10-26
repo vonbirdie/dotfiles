0e93a5f49bd7be1353082444290f0f3835dd279a
Andreas Falk <afalk@google.com>
2022-10-26 20:24:22 +0100

    Configure lualine sections.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,14 @@
 require('lualine').setup {
   options = {
     globalstatus = true,
-  }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
 }
 
