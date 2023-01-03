a5f50174b2bf51e7d54473af4102775b8af57582
Andreas Falk <mail@andreasfalk.se>
2023-01-03 23:29:28 +0000

    Replace nvim_set_keymap with vim.keymap.set calls

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,19 @@
 
-vim.api.nvim_set_keymap("", "<c-o>", "<cmd>lua require('telescope.builtin').resume()<cr>", {})
+vim.keymap.set("", "<c-o>", "<cmd>lua require('telescope.builtin').resume()<cr>")
 
-vim.api.nvim_set_keymap("", "<c-h>", "<cmd>lua require('telescope.builtin').help_tags()<cr>", {})
-vim.api.nvim_set_keymap("", "<c-k>", "<cmd>lua require('telescope.builtin').keymaps()<cr>", {})
+vim.keymap.set("", "<c-h>", "<cmd>lua require('telescope.builtin').help_tags()<cr>")
+vim.keymap.set("", "<c-k>", "<cmd>lua require('telescope.builtin').keymaps()<cr>")
 
-vim.api.nvim_set_keymap("", "<space>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", {})
-vim.api.nvim_set_keymap("", "<space>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", {})
-vim.api.nvim_set_keymap("", "<space>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", {})
-vim.api.nvim_set_keymap("", "<space>fm", "<cmd>lua require('telescope.builtin').marks()<cr>", {})
+vim.keymap.set("", "<space>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
+vim.keymap.set("", "<space>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+vim.keymap.set("", "<space>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
+vim.keymap.set("", "<space>fm", "<cmd>lua require('telescope.builtin').marks()<cr>")
 
-vim.api.nvim_set_keymap("", "gD", "<cmd>lua require('telescope.builtin').lsp_type_definitions()<cr>", {})
-vim.api.nvim_set_keymap("", "gd", "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>", {})
-vim.api.nvim_set_keymap("", "gi", "<cmd>lua require('telescope.builtin').lsp_implementations()<cr>", {})
-vim.api.nvim_set_keymap("", "gr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>", {})
+vim.keymap.set("", "gD", "<cmd>lua require('telescope.builtin').lsp_type_definitions()<cr>")
+vim.keymap.set("", "gd", "<cmd>lua require('telescope.builtin').lsp_definitions()<cr>")
+vim.keymap.set("", "gi", "<cmd>lua require('telescope.builtin').lsp_implementations()<cr>")
+vim.keymap.set("", "gr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>")
 
-vim.api.nvim_set_keymap("", "<space>ca", "<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>", {})
-vim.api.nvim_set_keymap("", "<space>cd", "<cmd>lua require('telescope.builtin').diagnostics()<cr>", {})
+vim.keymap.set("", "<space>ca", "<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>")
+vim.keymap.set("", "<space>cd", "<cmd>lua require('telescope.builtin').diagnostics()<cr>")
 
