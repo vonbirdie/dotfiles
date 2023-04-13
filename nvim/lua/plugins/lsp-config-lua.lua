cc9c13590a28f914e63204c52ebba07801ba8933
Andreas Falk <mail@andreasfalk.se>
2023-04-13 18:06:01 +0100

    Configure fidget via lsp config.

--- nvim/lua/plugins/lsp-config-lua.lua
+++ nvim/lua/plugins/lsp-config-lua.lua
@@ -2,6 +2,17 @@
 return {
   'neovim/nvim-lspconfig',
 
+  dependencies = {
+    -- Displays a fidget spinner indicating LSP progress.
+    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+    -- https://github.com/j-hui/fidget.nvim
+    {'j-hui/fidget.nvim', config = {}},
+
+    -- Neovim setup for init.lua with full signature help, docs and completion for the nvim lua API.
+    -- https://github.com/folke/neodev.nvim
+    'folke/neodev.nvim',
+  },
+
   config = function()
     -- Use an on_attach function to only map the following keys
     -- after the language server attaches to the current buffer
@@ -29,10 +40,6 @@ return {
       vim.diagnostic.goto_prev()
     end, { desc = "Previous diagnostic" })
 
-    local runtime_path = vim.split(package.path, ';')
-    table.insert(runtime_path, "lua/?.lua")
-    table.insert(runtime_path, "lua/?/init.lua")
-
     -- nvim-cmp almost supports LSP's capabilities so advertise it to LSP servers.
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
@@ -41,16 +48,6 @@ return {
       capabilities = capabilities,
       settings = {
         Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-            -- Setup your lua path
-            path = runtime_path,
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
-          },
           workspace = {
             -- Make the server aware of Neovim runtime files
             library = vim.api.nvim_get_runtime_file("", true),
