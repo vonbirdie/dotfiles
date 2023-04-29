a5ffcb21d64dadd2fdbc4659993dc9086eadfd73
Andreas Falk <mail@andreasfalk.se>
2023-04-29 13:21:47 +0100

    Remove posix-source fish script.

--- fish/functions/posix-source.fish
+++
@@ -1,9 +0,0 @@
-
-function posix-source
-  for line in (cat $argv | grep --invert-match --line-regexp --perl-regexp --regexp="[ \t]*(#.+)?")
-    set varValues (echo $line |tr = \n)
-    set  --global --export $varValues[1] (echo $varValues[2] | sed -e 's/^"//' | sed -e 's/"$//')
-    set --show $varValues[1]
-  end
-end
-
