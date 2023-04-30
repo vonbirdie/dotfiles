c415669e532413efb683c049f5af0e8919a4aa5e
Andreas Falk <mail@andreasfalk.se>
2023-04-30 17:57:08 +0100

    Make friendly-snippets a dependency of LuaSnip.

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -13,9 +13,14 @@ return {
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lsp-signature-help',
 
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'rafamadriz/friendly-snippets',
+    {
+      'L3MON4D3/LuaSnip',
+
+      dependencies = {
+        'saadparwaiz1/cmp_luasnip',
+        'rafamadriz/friendly-snippets',
+      },
+    },
   },
 
   config = function()
