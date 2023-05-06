5ebf4625d968eddc189789b18a206e33fc980008
Andreas Falk <mail@andreasfalk.se>
2023-05-06 11:22:35 +0100

    Remove treesitter textobjects and treesitter context.

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,13 +2,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
 
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-
-    -- Evaluate as of 13/04/23.
-    'romgrk/nvim-treesitter-context',
-  },
-
   build = ':TSUpdate' ,
 
   lazy = false,
@@ -66,42 +59,6 @@ return {
 
     indent = {
       enable = true
-    },
-
-    textobjects = {
-      select = {
-        enable = true,
-
-        lookahead = true,
-
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-        },
-      },
-
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
-        },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
-      },
     },
   }
 }
